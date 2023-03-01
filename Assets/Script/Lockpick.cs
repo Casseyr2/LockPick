257f2cc100c82785560194bb95f637056d251760
AMullin <amullin3@student.fitchburgstate.edu>
2023-03-01 01:02:59 -0500

    Lock Pick finished
    Finished the lock pick controls.
    
    Controls are based on the second touch. you can swipe left or right to move the pick, but only before you have moved it up or down too much.

--- Assets/Script/Lockpick.cs
+++ Assets/Script/Lockpick.cs
@@ -12,6 +12,7 @@ public class Lockpick : MonoBehaviour
     public int swipeToleranceX; //swipe left/right to move
     public int swipeToleranceY; //if outside of this tolerance, swipe is disabled.
     bool fingerDown = false;
+    bool inTolerance;
     Vector2 touchStartPos;
 
     //Pin related. Pick will snap to pin locations on swips. Positions[0] is just in front of the lock
@@ -27,12 +28,26 @@ public class Lockpick : MonoBehaviour
     Vector3 corePosition; //Y position of core, determined by pick starting position
     Vector3 positionLastUpdate;
 
+    //will contain the boundaries of the screen in world space
+    //used in converting pixel movement to unit movement of pick
+    Vector3 screenBoundaryMin;
+    Vector3 screenBoundaryMax;
+    float screenHeightY;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         corePosition = new Vector3(0, transform.position.y, 0);
         pickPositionOffset = offsetObject.transform.position - transform.position;
 
+        Camera cam = Camera.main;
+        screenBoundaryMin = cam.ScreenToWorldPoint(Vector3.zero);
+        screenBoundaryMax = cam.ScreenToWorldPoint(new Vector3(cam.pixelWidth, cam.pixelHeight, 0));
+
+        screenHeightY = screenBoundaryMax.y - screenBoundaryMin.y;
+
+        moveToPin(currentPin);
     }
 
     // Update is called once per frame
@@ -47,6 +62,7 @@ public class Lockpick : MonoBehaviour
                 touchStartPos = Input.touches[1].position;
                 Debug.Log("Pick Activated");
 
+                inTolerance = true;
                 positionLastUpdate = touchStartPos;
             }
 
@@ -55,13 +71,31 @@ public class Lockpick : MonoBehaviour
             if (fingerDown)
             {
                 //move Pick up and down in response to finger movement
-                
+
+                //movement calculates the proportion of 1 pixel of movement = how many units in world space?
+                float movement = (screenHeightY * (Input.touches[1].position.y - positionLastUpdate.y)) / Camera.main.pixelHeight;
+                transform.Translate(new Vector3(0, movement, 0) );
+
+                if (Mathf.Abs(Input.touches[1].position.y - touchStartPos.y) > swipeToleranceY)
+                {
+                    inTolerance = false;
+                }
+
                 //If within toleranceY, check to see if swipe.
-                    //On swipe, move pick to next location and then disable fingerdown
-
-
+                //On swipe, move pick to next location and then disable fingerdown
+                if (inTolerance && Input.touches[1].position.x - touchStartPos.x > swipeToleranceX)
+                {
+                    moveToPin(currentPin + 1);
+                }
+                if (inTolerance && Input.touches[1].position.x - touchStartPos.x < -swipeToleranceX)
+                {
+                    moveToPin(currentPin - 1);
+                }
+                    
 
             }
+
+            positionLastUpdate = Input.touches[1].position;
         }
 
         //Finger has been released, deactivate the pick
@@ -70,12 +104,15 @@ public class Lockpick : MonoBehaviour
         {
             fingerDown = false;
 
+            moveToPin(currentPin);
+
             Debug.Log("Pick Deactivated");
         }
 
 
     }
 
+    //Moves the pick to a new spot and disables fingerDown to end the touch.
     void moveToPin(int pin)
     {
         //Don't move out of boudns
@@ -92,6 +129,9 @@ public class Lockpick : MonoBehaviour
                 - pickPositionOffset;
 
 
+            fingerDown = false;
+            Debug.Log("Pick deactivated after moving");
+            currentPin = pin;
         }
     }
 }
